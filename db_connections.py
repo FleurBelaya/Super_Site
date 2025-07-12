1c0e51977f2ac7775809ad8fff82c64f4215a0c1
Polina Belaya <polinabeline@gmail.com>
2025-07-12 23:06:34 +0300

    generate app

--- db_connections.py
+++ db_connections.py
@@ -3,4 +3,14 @@ from sqlalchemy.orm import Session, sessionmaker
 from sqlalchemy import URL, create_engine, text
 from config import settings
 
+engine = create_engine(
+    url = settings.DATABASE_URL_psycopg,
+    echo=True,
+    pool_size=5,
+    max_overflow=10
+)
 
+
+with engine.connect() as conn:
+    res = conn.execute(text('SELECT VERSION()'))
+    print(f'{res.all()=}')

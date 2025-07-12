be0b45cda7f08d7ea94a3b097ff0d9ff081448eb
Polina Belaya <polinabeline@gmail.com>
2025-07-12 23:19:08 +0300

    generate app

--- db_connections.py
+++ db_connections.py
@@ -3,6 +3,8 @@ from sqlalchemy.orm import Session, sessionmaker
 from sqlalchemy import URL, create_engine, text
 from config import settings
 
+print(settings.DB_NAME)
+print(settings.DATABASE_URL_psycopg)
 engine = create_engine(
     url = settings.DATABASE_URL_psycopg,
     echo=True,
@@ -10,7 +12,13 @@ engine = create_engine(
     max_overflow=10
 )
 
+with engine.begin() as conn:
+    conn.execute(text("""
+        CREATE TABLE IF NOT EXISTS Cats (
+            id SERIAL PRIMARY KEY,
+            name TEXT NOT NULL,
+            age INTEGER
+        )
+    """))
 
-with engine.connect() as conn:
-    res = conn.execute(text('SELECT VERSION()'))
-    print(f'{res.all()=}')
+print('successful')
